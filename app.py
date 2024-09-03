d8d227f4c1652365ec2c06e8bfe258ba60fac46b
JeniaD <jeniadeshchenko@gmail.com>
2024-09-03 22:58:16 +0200

    messages

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
+app.config.from_object("config")
+
+@app.route("/debug")
+def debug():
+    return render_template("base.html")
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        login = request.args.get("login")
+        password = request.args.get("password")
+
+        flash("Login successful", "success")
+        return redirect(url_for("index"))
+
     return render_template("login.html")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+SECRET_KEY = "SECRET"
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
