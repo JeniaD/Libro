3667a9296745cbc8ee086711e70d55ba98aaaa9c
JeniaD <jeniadeshchenko@gmail.com>
2024-09-03 21:47:13 +0200

    added login form

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("base.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
